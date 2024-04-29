9a800b43fe4f76e166f5c001530c0e368ea37184
Weirdmeats <165041244+Weirdmeats@users.noreply.github.com>
2024-04-29 00:36:10 -0300

    Tarea: Proyecto pagina. Fecha: 24/04/2024. Versión: 2.4.

---
+++ sites/assets/js/trigger.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Función para mostrar la alerta cada 10 segundos
+    function mostrarAlertaCada10Segundos() {
+        // Selecciona la alerta por su ID
+        var alerta = document.getElementById('index_alert');
+        // Muestra la alerta
+        alerta.style.display = 'block';
+        // Configura un temporizador para ocultar la alerta después de 5 segundos
+        setTimeout(function() {
+            alerta.style.display = 'none';
+        }, 5000); // 5000 milisegundos = 5 segundos
+    }
+
+    // Llama a la función por primera vez para que la alerta se muestre de inmediato
+    mostrarAlertaCada10Segundos();
+
+    // Configura un intervalo para llamar a la función cada 100 segundos
+    setInterval(mostrarAlertaCada10Segundos, 100000); // 10000 milisegundos = 100 segundos
+});
